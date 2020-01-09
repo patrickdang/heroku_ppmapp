673c9561edabe2607c13a354052c1eca743c0110
Patrick Dang <patrickdang6356@gmail.com>
2020-01-08 19:18:16 -0600

    react

---
+++ src/main/resources/static/precache-manifest.b98bb47dc3d522449da1db3ad5aa257d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8227d3c7a30b4a42b300e188ade176c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7388ee969a85e5dbbe39",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "7388ee969a85e5dbbe39",
+    "url": "/static/js/main.38db1d8f.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
